9c209ed8fa72dbc29f4c6bd552215968e3b5c622
Seth-Cash <sethcash@mail.weber.edu>
2026-07-05 23:05:53 -0600

    Added Fee CRUD Pages, Fee Model, Reservation Model, ReservationFee Model, and FeeController

---
+++ Controllers/FeeController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class FeeController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public FeeController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Fees.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Fee fee)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Fees.Add(fee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(fee);
+    }
+
+    public async Task<IActionResult> Edit(int id)
+    {
+        var fee = await _context.Fees.FindAsync(id);
+        return View(fee);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(Fee fee)
+    {
+        _context.Update(fee);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var fee = await _context.Fees.FindAsync(id);
+        _context.Fees.Remove(fee);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
